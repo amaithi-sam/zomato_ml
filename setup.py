df3d6d6445fd5ebf9d046c3d287282b2a463f833
Amaithi <amaithichirasan.s@gmail.com>
2023-06-22 08:53:18 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Zomato Restaurant Rating Prediction",
+    version="1.0.0",
+    packages=find_packages(),
+    license="MIT",
+    author="Amaithi Chirasan"
+)
